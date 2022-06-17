ca30d116785a4e67bbfbcf08efc8d1acf99f338c
Vlad Rares Raducu <vlad.raducu@cloudflight.io>
2022-06-17 08:01:06 +0300

    Fixed animation slide-in bug and added slide-up animation from Javascript

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const MODAL_CSS_CLASS = ".modal";
+const MODAL_CONTENT_CLASS = ".modal-content";
 const BUTTON_CSS_CLASS = ".btn";
 const CLOSE_MODAL_CSS_CLASS = ".close-modal";
 
 const modal = document.querySelector(MODAL_CSS_CLASS);
 const button = document.querySelector(BUTTON_CSS_CLASS);
 const closeModalButton = document.querySelector(CLOSE_MODAL_CSS_CLASS);
+const modalContent = document.querySelector(MODAL_CONTENT_CLASS);
 
 function openModal(event) {
     event.preventDefault();
     modal.style.display = "block";
 }
 
-button.addEventListener("click", openModal);
-
-function closeModal(event) {
-    modal.style.display = "none";
+function closeModal() {
+    modalContent.classList.add("slide-up");
+    setTimeout(() => {
+        modal.style.display = "none";
+        modalContent.classList.remove("slide-up");
+    }, 500);
 }
 
+button.addEventListener("click", openModal);
 closeModalButton.addEventListener("click", closeModal);
 modal.addEventListener("click", closeModal);
